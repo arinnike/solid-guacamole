909c0bd960622a5b7d3117d07841bec2d157d630
arinnike <sarah.b.douglas@gmail.com>
2026-02-14 15:34:20 -0600

    Update settings.js

--- static/js/settings.js
+++ static/js/settings.js
@@ -16,9 +16,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const darkModeCheckbox = document.getElementById("dark-mode");
   const saveBtn = document.getElementById("save-settings");
   const status = document.getElementById("status");
-
+console.log("saveBtn:", saveBtn);
   const { data } = await supabase.auth.getSession();
-
   if (!data.session) {
     window.location.href = "/";
     return;
